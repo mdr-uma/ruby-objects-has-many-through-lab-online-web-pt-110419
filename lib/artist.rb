aeead71e03b07f98f1978c453b0e49fc0e3d872e
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-19 02:09:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,10 +17,11 @@ class Artist
   def genres
     songs.collect {|song| song.genre}
   end
-  # def songs
-  #   Song.all.select {|song| song.artist == self}
-  # end
-  #
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
   def new_song(name, genre)
     Song.new(name, self, genre)
 
