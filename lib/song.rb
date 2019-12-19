2302fff00a7cc68b2f62335cc7f4e8f856cbb5b6
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-19 02:10:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
     attr_accessor :name, :artist, :genre
     @@all = []
 
-    def initialize(name, genre)
+    def initialize(name, artist, genre)
       @name = name
       @artist = artist
       @genre = genre
