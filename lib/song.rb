ce05a76ec9cc62f139cd67ee095c75fee1411cf8
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-19 01:31:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,15 @@
 class Song
 
-attr_accessor :name
-@@all = []
+    attr_accessor :name
+    @@all = []
 
-def initialize(name)
-  @name = name
-  @@all << self
-  @songs = []
-end
+    def initialize(name)
+      @name = name
+      @@all << self
+      @songs = []
+    end
 
-def self.all
-  @@all
-end
+    def self.all
+      @@all
+    end
 end
